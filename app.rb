92b93abff8b6dfc469160e10e277dfc14a9b0323
queen-cece <ccezeonu3@gmail.com>
2018-07-31 17:17:20 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Welcome to my life!"
+end
+
+  get '/name' do
+  "My name is Cece! Just to let y'all know. Okurttt!!"
+  end
+  
+  get '/hometown' do
+    "I'm from Newark, New Jersey"
+  end
+    
+  get '/favorite-song' do
+    "Freeedom-Beyonce and Goodman-Neyo"
+  end
 end
